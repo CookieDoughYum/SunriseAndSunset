faf673eae2cd2cbf8f00ef1cee13c3e85b9bec8d
CookieDoughYum <nathaneric07@icloud.com>
2022-08-20 13:16:10 -0700

    Folder

---
+++ Sunrise and Sunset/Sunrise-and-Sunset/src/main/java/https/api/sunrisesunset/org/json/lat60/lng40/Sunrise/and/Sunset/SunriseAndSunsetApplication.java
@@ -0,0 +1,13 @@
+package https.api.sunrisesunset.org.json.lat60.lng40.Sunrise.and.Sunset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunriseAndSunsetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunriseAndSunsetApplication.class, args);
+	}
+
+}
---
+++ Sunrise and Sunset/Sunrise-and-Sunset/src/test/java/https/api/sunrisesunset/org/json/lat60/lng40/Sunrise/and/Sunset/SunriseAndSunsetApplicationTests.java
@@ -0,0 +1,13 @@
+package https.api.sunrisesunset.org.json.lat60.lng40.Sunrise.and.Sunset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunriseAndSunsetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
